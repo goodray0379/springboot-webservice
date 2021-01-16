2f3f0fffbc3577a3b6b639c9580c28bda7591895
heoyoungmin <goodray0379@gmail.com>
2021-01-16 23:57:25 +0900

    기본 테스트코드 생성

---
+++ src/main/java/com/devmin/springboot/webservice/Application.java
@@ -0,0 +1,11 @@
+package com.devmin.springboot.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
