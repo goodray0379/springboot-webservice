d141af4dd27e0036e1b024a7c436023f96feb646
heoyoungmin <goodray0379@gmail.com>
2021-04-01 23:02:50 +0900

    머스테치 세팅

---
+++ src/main/java/com/devmin/springboot/webservice/web/IndexController.java
@@ -0,0 +1,17 @@
+package com.devmin.springboot.webservice.web;
+
+import com.devmin.springboot.webservice.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
